04b2d481bd3b05a71a50bc6b01759b386f14882c
Aiden Flynn <aflynn@danestreet.com>
2020-11-24 13:36:30 -0500

    adding staging url

--- src/api-config.js
+++ src/api-config.js
@@ -2,8 +2,10 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname
 
-if(hostname === "localhost") {
+if(hostname === 'localhost') {
   backendHost = 'http://localhost:3001'
+} else if (hostname === 'yoga-frontend.herokuapp.com') {
+  backendHost = 'https://glacial-falls-04883.herokuapp.com'
 }
 
 export const API_ROOT = backendHost;
