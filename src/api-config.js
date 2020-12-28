f66289c7dd987699f22e80604b57be9be19f58c1
awf825 <faiden454@gmail.com>
2020-12-28 18:07:42 -0500

    renaming api source

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname
 if(hostname === 'localhost') {
   backendHost = 'http://localhost:3001'
 } else if (hostname === 'yoga-frontend.herokuapp.com') {
-  backendHost = 'https://glacial-falls-04883.herokuapp.com'
+  backendHost = 'https://vyogi.herokuapp.com'
 }
 
 export const API_ROOT = backendHost;
