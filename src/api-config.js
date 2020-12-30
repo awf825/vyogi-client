6fc14e41679fab7476e6dc4fe0558b3b49c65db8
awf825 <faiden454@gmail.com>
2020-12-29 19:54:17 -0500

    local server for static deploys

--- src/api-config.js
+++ src/api-config.js
@@ -1,11 +1,14 @@
-let backendHost;
+// let backendHost;
+//
+// const hostname = window && window.location && window.location.hostname
+//
+// if(hostname === 'localhost') {
+//   backendHost = 'http://localhost:3001'
+// } else if (hostname === 'yoga-frontend.herokuapp.com') {
+//   backendHost = 'https://vyogi.herokuapp.com'
+// }
 
-const hostname = window && window.location && window.location.hostname
-
-if(hostname === 'localhost') {
-  backendHost = 'http://localhost:3001'
-} else if (hostname === 'yoga-frontend.herokuapp.com') {
-  backendHost = 'https://vyogi.herokuapp.com'
-}
+// for static deploying
+const backendHost = 'http://localhost:3001'
 
 export const API_ROOT = backendHost;
