a255caf3e9cdc2a66489d131c0afc4df6b6fe972
awf825 <faiden454@gmail.com>
2021-01-02 16:51:39 -0500

    workaround for undecided hostname

--- src/api-config.js
+++ src/api-config.js
@@ -4,9 +4,12 @@ const hostname = window && window.location && window.location.hostname
 
 if(hostname === 'localhost') {
   backendHost = 'http://localhost:3001'
-} else if (hostname === 'www.yogastaging.net') {
+} else {
   backendHost = 'https://vyogi.herokuapp.com'
 }
+// } else if (hostname === 'www.yogastaging.net') {
+//   backendHost = 'https://vyogi.herokuapp.com'
+// }
 
 // for static deploying
 // const backendHost = 'http://localhost:3001'
