aaa58abcca82720cafd712159ee667a74b5630c3
Nathan Mailhiot <nathanjm12@outlook.com>
2021-02-08 11:02:34 -0500

    Login backend error handling

--- src/components/Registration/RegistrationLogInForm.js
+++ src/components/Registration/RegistrationLogInForm.js
@@ -1,14 +1,16 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import axios from "axios";
 import { API_ROOT } from "../../api-config.js";
 import { authenticate } from "./RegistrationAuth";
 import { useHistory } from "react-router-dom";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Alert } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 
 const RegistrationLoginForm = () => {
   // Initial State
   const { register, handleSubmit, errors, watch } = useForm();
+  const [showErrors, setShowErrors] = useState(false);
+  const [errorHandling, setErrorHandling] = useState("");
   const history = useHistory();
   const password = useRef({});
   password.current = watch("password", "");
@@ -16,28 +18,29 @@ const RegistrationLoginForm = () => {
   // Login User function has not been tested.  We can change it however you want.
   const loginUser = async (data) => {
     try {
+      if (errorHandling.length >= 1) {
+        setShowErrors(false);
+        setErrorHandling("");
+      }
+
       const resp = await axios.post(`${API_ROOT}/signin`, data);
       if (resp) authenticate(resp.data, () => history.push("/"));
     } catch (err) {
-      console.error(err);
+      setErrorHandling(err.response.statusText);
+      setShowErrors(true);
     }
   };
 
-  // const loginUser = async (data) => {
-  //   axios
-  //     .post(`${API_ROOT}/signin`, data)
-  //     .then((resp) => {
-  //       if (resp.data.token) {
-  //         localStorage.setItem("token", resp.data.token);
-  //       }
-  //     })
-  //     .then((_) => history.push("/"))
-  //     .catch((err) => console.log(err));
-  // };
+  if (showErrors) {
+    return (
+      <Alert variant="danger" onClose={() => setShowErrors(false)} dismissible>
+        <Alert.Heading>{errorHandling}</Alert.Heading>
+      </Alert>
+    );
+  }
 
   function onSubmit(data) {
     loginUser(data);
-    history.push("/");
   }
 
   // Regex that checks to make sure the email is an email address.
@@ -47,41 +50,43 @@ const RegistrationLoginForm = () => {
   }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <Form.Group>
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          ref={register({
-            required: true,
-            validate: (input) => isEmail(input),
-          })}
-          name="email"
-          type="email"
-          placeholder="Email"
-        />
-        <h1>{errors.email?.type === "required" && "Email is required!"}</h1>
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          ref={register({
-            requred: true,
-            minLength: {
-              value: 5,
-              message: "Password is too short!",
-            },
-          })}
-          name="password"
-          type="password"
-          placeholder="Password"
-        />
-        <h1>
+    <>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <h4 style={{ color: "red" }}>
+          {errors.email?.type === "required" && "Email is required!"}
           {errors.password?.type === "required" && "Password is required!"}
           {errors.password?.type === "minLength" && errors.password?.message}
-        </h1>
-      </Form.Group>
-      <Button type="submit">Submit</Button>
-    </Form>
+        </h4>
+        <Form.Group>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            ref={register({
+              required: true,
+              validate: (input) => isEmail(input),
+            })}
+            name="email"
+            type="email"
+            placeholder="Email"
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            ref={register({
+              requred: true,
+              minLength: {
+                value: 5,
+                message: "Password is too short!",
+              },
+            })}
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </>
   );
 };
 
