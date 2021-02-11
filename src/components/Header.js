d1f7addfe3d5308c10ea66c9632480d973566acd
awf825 <faiden454@gmail.com>
2021-02-10 20:51:12 -0500

    housekeeping commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,36 +4,7 @@ import { useHistory } from "react-router-dom";
 import { signout } from "./Registration/RegistrationAuth";
 import { GoPerson } from "react-icons/go";
 
-// import { API_ROOT } from "../api-config.js";
-// import axios from "axios";
-
 export const Header = (props) => {
-  // Logout right now makes a call to backend that
-  // simply sends back 204. The alert at line 18
-  // is what you want to target. For now, don't worry about this
-  // particular error handle.
-
-  // const logout = () => {
-  //   axios
-  //     .post(`${API_ROOT}/signout`)
-  //     .then((resp) => {
-  //       if (resp.status === 204) {
-  //         alert("goodbye");
-  //       }
-  //     })
-  //     .then((_) => {
-  //       localStorage.clear();
-  //     })
-  //     .then((_) => {
-  //       props.history.push("/");
-  //     })
-  //     .catch((err) => {
-  //       console.log("ERROR AT SIGNOUT:", err);
-  //     });
-  // };
-
-  // I commented out the original signout and abstracted it to the signout method.
-  // I also added an Icon for the registration section let me know what you think.
   const history = useHistory();
   const logout = () => {
     signout(() => history.pushState("/"));
@@ -56,9 +27,6 @@ export const Header = (props) => {
             </Nav.Link>
           </Nav.Item>
         ) : (
-          // <Nav.Item>
-          //   <Nav.Link href="/register">Log In</Nav.Link>
-          // </Nav.Item>
           <Nav.Item>
             <Nav.Link href="/registration">
               <GoPerson />
