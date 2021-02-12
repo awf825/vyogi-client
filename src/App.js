c8d2a612815f2cd03210608ee4031a6fb2aeb4e8
Nathan Mailhiot <nathanjm12@outlook.com>
2021-02-12 16:05:12 -0500

    Navigation with styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Header";
+import Navigation from "./components/Navigation";
 import Video from "./components/Video";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Schedule from "./components/Schedule";
@@ -14,13 +15,26 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Navigation />
         <Switch>
           <Route exact path="/" render={(props) => <Dashboard {...props} />} />
           <Route exact path="/registration" component={RegistrationRouter} />
-          <Route exact path="/registration/login" component={RegistrationLogin}/>
-          <Route exact path="/registration/signup" component={RegistrationSignUp}/>
+          <Route
+            exact
+            path="/registration/login"
+            component={RegistrationLogin}
+          />
+          <Route
+            exact
+            path="/registration/signup"
+            component={RegistrationSignUp}
+          />
           <Route exact path="/video" render={(props) => <Video {...props} />} />
-          <Route eact path="/schedule" render={(props) => <Schedule {...props} />}/>
+          <Route
+            eact
+            path="/schedule"
+            render={(props) => <Schedule {...props} />}
+          />
         </Switch>
       </div>
     </BrowserRouter>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { signout } from "./Registration/RegistrationAuth";
 import {
   IoIosMenu,
   IoIosHome,
@@ -8,12 +9,81 @@ import {
   IoMdPerson,
   IoIosCloseCircleOutline,
 } from "react-icons/io";
+import { IoLogOutOutline } from "react-icons/io5";
 
 const Navigation = () => {
   const [open, setOpen] = useState(false);
+  const history = useHistory();
+
+  // For the nav to check if there is a user or not
+  const user = localStorage.getItem("token");
+
+  const logout = () => {
+    signout(() => history.push("/"));
+  };
+
+  const authorizeSelection = (e) => {
+    if (!localStorage.getItem("token")) {
+      // this is one way of doing this, but it freezes the button and
+      // makes shit look stupid
+      e.preventDefault();
+      alert("Nuh uh uh!");
+      return history.push("/");
+    }
+  };
+
   return (
     <div className="navigation">
-      <h1>Nav</h1>
+      {open ? (
+        <div className="navigation__navbar">
+          <div onClick={() => setOpen(false)}>
+            <IoIosCloseCircleOutline className="navigation__closer" />
+
+            <div className="navigation__scale">
+              <Link to="/" className="navigation__links">
+                <IoIosHome className="navigation__icon navigation__home" />
+              </Link>
+            </div>
+
+            <div className="navigation__scale">
+              <Link
+                to="/video"
+                onClick={(e) => authorizeSelection(e)}
+                className="navigation__links"
+              >
+                <IoIosTv className="navigation__icon navigation__vid" />
+              </Link>
+            </div>
+
+            <div className="navigation__scale">
+              <Link
+                to="/schedule"
+                onClick={(e) => authorizeSelection(e)}
+                className="navigation__links"
+              >
+                <IoIosCalendar className="navigation__icon navigation__cal" />
+              </Link>
+            </div>
+            {user ? (
+              <div className="navigation__scale">
+                <Link to="/" className="navigation__links" onClick={logout}>
+                  <IoLogOutOutline className="navigation__icon navigation__reg" />
+                </Link>
+              </div>
+            ) : (
+              <div className="navigation__scale">
+                <Link to="/registration" className="navigation__links">
+                  <IoMdPerson className="navigation__icon navigation__reg" />
+                </Link>
+              </div>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div onClick={() => setOpen(true)}>
+          <IoIosMenu className="navigation__opener" />
+        </div>
+      )}
     </div>
   );
 };
