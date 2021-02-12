4837d8135be155578b5b8949e1472240cc1671a7
Nathan Mailhiot <nathanjm12@outlook.com>
2021-02-12 15:31:01 -0500

    Navigation Component

---
+++ src/Navigation.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  IoIosMenu,
+  IoIosHome,
+  IoIosTv,
+  IoIosCalendar,
+  IoMdPerson,
+  IoIosCloseCircleOutline,
+} from "react-icons/io";
+
+const Navigation = () => {};
+
+export default Navigation;
