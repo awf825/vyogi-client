2f0d3723ca3f43035d7eec15c40085bb0678445c
Nathan Mailhiot <nathanjm12@outlook.com>
2021-02-15 13:10:13 -0500

    Contact form with just message field

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -19,58 +19,9 @@ const Contact = () => {
       <div className="contact__form_container">
         <Form className="contact__form">
           <Form.Group>
-            <Form.Label>First Name</Form.Label>
-            <Form.Control
-              ref={register({
-                required: {
-                  value: true,
-                  message: "First name is required!",
-                },
-                minLength: {
-                  value: 2,
-                  message: "First name is too short!",
-                },
-              })}
-              name="firstname"
-              type="text"
-              placeholder="First Name"
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control
-              ref={register({
-                required: {
-                  value: true,
-                  message: "Last name is required!",
-                },
-                minLength: {
-                  value: 1,
-                  message: "Last name is too short!",
-                },
-              })}
-              name="lastname"
-              type="text"
-              placeholder="Last Name"
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              ref={register({
-                required: {
-                  value: true,
-                  message: "Email is required!",
-                },
-                validate: (input) => isEmail(input),
-              })}
-              name="email"
-              type="email"
-              placeholder="Email"
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Message</Form.Label>
+            <Form.Label className="contact__label">
+              Send us a message!
+            </Form.Label>
             <Form.Control
               as="textarea"
               ref={register({
