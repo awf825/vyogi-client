35f93479f0ce35ba9f2769227968991d03dcc381
awf825 <faiden454@gmail.com>
2021-03-08 22:34:17 -0500

    added bio and facebook link need smaller picture

--- src/components/About.js
+++ src/components/About.js
@@ -9,45 +9,33 @@ import {
 const About = () => {
   return (
     <div className="about">
-      <h1 className="about__heading">About Us</h1>
       <div className="about__main">
         <div className="about__img"></div>
-        <h2 className="about__instructor">Testname & Test</h2>
+        <h2 className="about__instructor">Hi I’m Alanna!</h2>
         <p className="about__text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-          ut augue vel nisl gravida sagittis. Donec sollicitudin egestas orci,
-          et dignissim ex commodo ut. Donec scelerisque hendrerit risus et
-          maximus. Nam et dictum magna. Donec ac suscipit ex. Donec suscipit
-          lorem ac ex volutpat, sit amet bibendum velit elementum. Quisque
-          mattis scelerisque dolor, sit amet eleifend mauris efficitur vitae. In
-          ut erat quis odio vulputate lobortis id ut lorem. Sed sit amet arcu in
-          odio mattis malesuada. Curabitur porta, magna at vestibulum eleifend,
-          risus est tincidunt erat, id venenatis urna elit at erat. Phasellus
-          rutrum dictum elit sit amet volutpat. Duis ligula dui, scelerisque
-          vitae pharetra id, congue dapibus eros. Etiam at odio venenatis,
-          commodo odio at, posuere turpis. Phasellus mollis auctor libero, non
-          mollis quam dapibus in. Ut eu nisi eget velit laoreet cursus. Fusce at
-          dictum justo. Ut ante enim, malesuada dignissim mauris ac, sodales
-          rutrum dui. Fusce vitae felis a nibh interdum tempor. Vivamus
-          vulputate at augue non molestie. Sed eget eros nisi. Quisque congue,
-          libero vel auctor suscipit, turpis nisl tincidunt purus, at tempor
-          urna ligula et turpis. Vestibulum ante ipsum primis in faucibus orci
-          luctus et ultrices posuere cubilia curae; Nulla ligula ipsum, pharetra
-          et congue id, vulputate eu augue. Ut fermentum, lorem eu sollicitudin
-          pretium, arcu elit ultricies lacus, et varius quam tortor at enim.
-          Aenean purus turpis, sagittis vel ullamcorper sed, volutpat eu purus.
-          Cras viverra, diam a aliquet placerat, justo lectus mollis quam, in
-          imperdiet enim orci mattis lectus. Pellentesque eu ligula dignissim
-          eros mollis tempus id vitae nunc. Nunc eget vulputate neque, a aliquam
-          nisi. Suspendisse potenti. Class aptent taciti sociosqu ad litora
-          torquent per conubia nostra, per inceptos himenaeos. Donec a elit
-          tincidunt, rutrum tellus ac, aliquam elit. Morbi gravida arcu sit amet
-          magna lacinia, eget fringilla arcu tincidunt. Donec porttitor turpis
-          at lorem rhoncus, ac venenatis mi interdum. Morbi in varius urna.
+        My first experience with yoga was all the way 
+        back in elementary school when I found a Rodney Yee DVD at 
+        the mall and decided to give it a try. From that point on 
+        I dabbled here and there, but once I hit high school found 
+        that I was intimidated by the competitive environment of many 
+        yoga studios. It wasn’t until a couple years ago, when I was 
+        living in New York City, that I began to practice seriously again. 
+        I turned to yoga as a way to calm down in the hectic environment, 
+        and this time it really stuck! I decided that yoga had such a 
+        positive impact on her wellbeing that I had to share with others. 
+        Last year I headed to the Kripalu Center to get her 200 hour 
+        certification, drawn to the Kripalu style due to its emphasis 
+        on compassion and inclusiveness. I believe yoga is truly for 
+        everybody regardless of size, age, background, or athletic ability 
+        and I try to live and teach by Swami Kripalu’s words: “The 
+        highest form of spiritual practice is self-observation without judgement.” 
+        When I’m not practicing yoga, you can find me cooking, reading, 
+        playing piano, (attempting) to learn new languages, or 
+        watching Real Housewives. 
         </p>
         <ul className="about__list">
           <li className="about_item">
-            <a className="about__link" href="#">
+            <a className="about__link" href="https://www.facebook.com/alanna.flynn.54">
               <IoLogoFacebook className="about__icon about__fb" />
             </a>
           </li>
