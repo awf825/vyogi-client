d78ce390c1c9b4e2edf4cef621d4039f2c4307f6
Nathan Mailhiot <nathanjm12@outlook.com>
2021-04-08 16:50:40 -0400

    Lessons form responsive

--- src/components/LessonForm.js
+++ src/components/LessonForm.js
@@ -102,110 +102,112 @@ const LessonForm = () => {
         <div className="lessonForm">
           <div className="lessonForm__form__container">
             <Form className="lessonForm__form" onSubmit={handleSubmit}>
-              <h3 className="lessonForm__heading">Lesson Form</h3>
-              <Form.Group>
-                <Form.Label className="lessonForm__label">
-                  Are you pregnant/have you given birth within the past year?
-                </Form.Label>
-                <span>{errors.check}</span>
-                <Form.Group className="lessonForm__radio">
-                  <Form.Check
-                    className="lessonForm__radio__btn"
-                    type="radio"
-                    value="yes"
-                    checked={data.check === "yes"}
-                    label="Yes"
-                    name="check"
+              <div className="lessonForm__content">
+                <h3 className="lessonForm__heading">Lesson Form</h3>
+                <Form.Group>
+                  <Form.Label className="lessonForm__label">
+                    Are you pregnant/have you given birth within the past year?
+                  </Form.Label>
+                  <span>{errors.check}</span>
+                  <Form.Group className="lessonForm__radio">
+                    <Form.Check
+                      className="lessonForm__radio__btn"
+                      type="radio"
+                      value="yes"
+                      checked={data.check === "yes"}
+                      label="Yes"
+                      name="check"
+                      onChange={handleChange}
+                    />
+                    <Form.Check
+                      className="lessonForm__radio__btn"
+                      type="radio"
+                      value="no"
+                      label="No"
+                      name="check"
+                      checked={data.check === "no"}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label className="lessonForm__label">
+                    Have you practiced yoga before? If so, what type and for how
+                    long?
+                  </Form.Label>
+                  <span>
+                    {errors.practiced !== "Tell us about you!"
+                      ? errors.practiced
+                      : ""}
+                  </span>
+                  <Form.Control
+                    className="lessonForm__text"
+                    as="textarea"
+                    name="practiced"
+                    rows={2}
+                    value={data.practiced}
                     onChange={handleChange}
-                  />
-                  <Form.Check
-                    className="lessonForm__radio__btn"
-                    type="radio"
-                    value="no"
-                    label="No"
-                    name="check"
-                    checked={data.check === "no"}
-                    onChange={handleChange}
+                    placeholder={errors.practiced}
                   />
                 </Form.Group>
-              </Form.Group>
-              <Form.Group>
-                <Form.Label className="lessonForm__label">
-                  Have you practiced yoga before? If so, what type and for how
-                  long?
-                </Form.Label>
-                <span>
-                  {errors.practiced !== "Tell us about you!"
-                    ? errors.practiced
-                    : ""}
-                </span>
-                <Form.Control
-                  className="lessonForm__text"
-                  as="textarea"
-                  name="practiced"
-                  rows={2}
-                  value={data.practiced}
-                  onChange={handleChange}
-                  placeholder={errors.practiced}
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label className="lessonForm__label">
-                  Do you have any injuries or physical limitations?
-                </Form.Label>
-                <span>
-                  {errors.limitations !== "Tell us about you!"
-                    ? errors.limitations
-                    : ""}
-                </span>
-                <Form.Control
-                  as="textarea"
-                  className="lessonForm__text"
-                  name="limitations"
-                  rows={2}
-                  value={data.limitations}
-                  onChange={handleChange}
-                  placeholder={errors.limitations}
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label className="lessonForm__label">
-                  Is there anything specific you'd like to focus on during our
-                  time together?
-                </Form.Label>
-                <span>
-                  {errors.focus !== "Tell us about you!" ? errors.focus : ""}
-                </span>
-                <Form.Control
-                  as="textarea"
-                  className="lessonForm__text"
-                  name="focus"
-                  rows={2}
-                  value={data.focus}
-                  onChange={handleChange}
-                  placeholder={errors.focus}
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label className="lessonForm__label">
-                  Is there anything else you'd like me to know?
-                </Form.Label>
-                <span>
-                  {errors.needToKnow !== "Tell us about you!"
-                    ? errors.needToKnow
-                    : ""}
-                </span>
-                <Form.Control
-                  as="textarea"
-                  className="lessonForm__text"
-                  name="needToKnow"
-                  rows={2}
-                  value={data.needToKnow}
-                  onChange={handleChange}
-                  placeholder={errors.needToKnow}
-                />
-              </Form.Group>
-              <Button type="submit">Submit</Button>
+                <Form.Group>
+                  <Form.Label className="lessonForm__label">
+                    Do you have any injuries or physical limitations?
+                  </Form.Label>
+                  <span>
+                    {errors.limitations !== "Tell us about you!"
+                      ? errors.limitations
+                      : ""}
+                  </span>
+                  <Form.Control
+                    as="textarea"
+                    className="lessonForm__text"
+                    name="limitations"
+                    rows={2}
+                    value={data.limitations}
+                    onChange={handleChange}
+                    placeholder={errors.limitations}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label className="lessonForm__label">
+                    Is there anything specific you'd like to focus on during our
+                    time together?
+                  </Form.Label>
+                  <span>
+                    {errors.focus !== "Tell us about you!" ? errors.focus : ""}
+                  </span>
+                  <Form.Control
+                    as="textarea"
+                    className="lessonForm__text"
+                    name="focus"
+                    rows={2}
+                    value={data.focus}
+                    onChange={handleChange}
+                    placeholder={errors.focus}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label className="lessonForm__label">
+                    Is there anything else you'd like me to know?
+                  </Form.Label>
+                  <span>
+                    {errors.needToKnow !== "Tell us about you!"
+                      ? errors.needToKnow
+                      : ""}
+                  </span>
+                  <Form.Control
+                    as="textarea"
+                    className="lessonForm__text"
+                    name="needToKnow"
+                    rows={2}
+                    value={data.needToKnow}
+                    onChange={handleChange}
+                    placeholder={errors.needToKnow}
+                  />
+                </Form.Group>
+                <Button type="submit">Submit</Button>
+              </div>
             </Form>
           </div>
         </div>
